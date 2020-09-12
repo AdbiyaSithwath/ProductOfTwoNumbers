1cf9f6a7fa059f8401c6b98062d6262e8085beff
AdbiyaSithwath <66269369+AdbiyaSithwath@users.noreply.github.com>
2020-09-12 11:57:55 +0530

    Product.cpp
    This file prints the product of 2 numbers.
    
    Thank you!

---
+++ Product.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+   double n1,n2,productOfTwoNumbers; 
+   cout << "Enter two numbers: "; 
+   // n1=3.4 n2=5.5
+   cin >> n1 >> n2; 
+   productOfTwoNumbers = n1 * n2; 
+   cout << "Product = " << productOfTwoNumbers; 
+   return 0; 
+}
